2a4eab7500ffd92a417596ca37bc8fd2b0a16c4f
feizai1012 <1429758925@qq.com>
2023-02-21 16:46:54 +0800

    111

---
+++ src/java/com/qy/Main.java
@@ -0,0 +1,11 @@
+package com.qy;
+
+/**
+ * Author:冯嘉裕
+ * Date2023/2/21 16:46
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("========");
+    }
+}
